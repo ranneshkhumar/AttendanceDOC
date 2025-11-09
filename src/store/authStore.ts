ccffaf87e5cbbbd5d8741579bd92e3072a33c77b
Rannesh khumar B R <rannesh.br@gmail.com>
2025-11-09 22:48:22 +0530

    Update authStore.ts

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -5,11 +5,19 @@ interface AuthState {
   user: User | null
   setUser: (user: User | null) => void
   logout: () => void
+
+  attendanceCache: any[]          // <-- NEW
+  setAttendanceCache: (rows: any[]) => void  // <-- NEW
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
   logout: () => set({ user: null }),
+
+  attendanceCache: [],                     // <-- NEW
+  setAttendanceCache: (rows) => set({ attendanceCache: rows }),  // <-- NEW
 }))
+
+
 
